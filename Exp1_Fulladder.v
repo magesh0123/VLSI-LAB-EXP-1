2695a9f8f99444919f888cae450e4f10078fdaa5
magesh0123 <162102402+magesh0123@users.noreply.github.com>
2024-04-01 13:27:46 +0530

    Update Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,8 +1,10 @@
-module fa_ha(a,b,c,sum,carry);
-input a,b,c;
-output sum,carry;
-
-
-
-
+module fulladder_c(s,co,x,y,ci);
+input x,y,ci;
+output s,co;
+wire w1,w2,w3;
+xor G1(w1,x,y);
+xor G2(s,w1,ci);
+and G3(w2,w1,ci);
+and G4(w3,x,y);
+or G5(co,w2,w3);
 endmodule
